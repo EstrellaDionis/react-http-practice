4a2390115fc800a8fcdd4830a80c37645c96dd90
EstrellaDionis <EstrellaDionis@gmail.com>
2022-01-08 17:39:40 -0500

    create PostList component to begin practice on http requests

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PostList extends Component {
+    render() {
+        return (
+            <div>
+                Lists of Posts
+            </div>
+        )
+    }
+}
+
+export default PostList
